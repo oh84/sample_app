f7a02baf4ee2526d651357aeba9b1eee2310bcab
oh84 <100363393+oh84@users.noreply.github.com>
2024-06-09 22:38:53 +0900

    root_urlでhomeページを表示するように変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "application#hello"
 end
