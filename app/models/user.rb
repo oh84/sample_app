a92beebfdc46657aa182274299539ca5038507a6
oh84 <100363393+oh84@users.noreply.github.com>
2024-06-23 21:10:30 +0900

    バリデーションを追加してメールアドレスの一意性を検証する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
 end
