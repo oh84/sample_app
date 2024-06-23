1134d5ceb8a55259ced7c13510f9c90d2f42373e
oh84 <100363393+oh84@users.noreply.github.com>
2024-06-23 17:08:00 +0900

    バリデーションを追加してメールアドレスの形式を検証する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
