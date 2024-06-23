3e0b89d1aa13f26825c8da16bdb2c9bfe5f80704
oh84 <100363393+oh84@users.noreply.github.com>
2024-06-24 00:13:40 +0900

    フォーム送信時のユーザー登録処理の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
 end
