3d3f83afd0c17e10b2fa6c4cb75f28fb8009ddbc
oh84 <100363393+oh84@users.noreply.github.com>
2024-06-27 23:02:39 +0900

    テストをパスするように修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザーページにリダイレクトする
     else
-      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
     end
   end
