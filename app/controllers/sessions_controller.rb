0fca025fa2015c6c1ae550f752dd72cc5f9d662b
oh84 <100363393+oh84@users.noreply.github.com>
2024-06-27 22:55:58 +0900

    ログインフォームを作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザーページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
 end
