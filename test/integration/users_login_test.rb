92b20cbacfe98bc6d9e680e0a002ef6b41de264e
oh84 <100363393+oh84@users.noreply.github.com>
2024-08-15 12:46:26 +0900

    ログインテストをリファクタ

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,13 +1,18 @@
 require 'test_helper'
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
+class UsersLogin < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
+end
 
-  test 'login with invalid email / invalid password' do
+class InvalidPasswordTest < UsersLogin
+  test 'login path' do
     get login_path
     assert_template 'sessions/new'
+  end
+
+  test 'login with invalid email / invalid password' do
     post login_path, params: { session: { email: '', password: '' } }
     assert_not is_logged_in?
     assert_template 'sessions/new'
@@ -17,8 +22,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test 'login with valid email / invalid password' do
-    get login_path
-    assert_template 'sessions/new'
     post login_path, params: { session: { email: @user.email, password: 'invalid' } }
     assert_not is_logged_in?
     assert_template 'sessions/new'
@@ -26,20 +29,45 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty? # 移動先のページでフラッシュメッセージが表示されていないことを確認する
   end
+end
 
-  test 'login with valid information followed by logout' do
+class ValidLogin < UsersLogin
+  def setup
+    super
     post login_path, params: { session: { email: @user.email, password: 'password' } }
+  end
+end
+
+class ValidLoginTest < ValidLogin
+  test 'valid login' do
+    assert is_logged_in?
     assert_redirected_to @user
+  end
+
+  test 'redirect after login' do
     follow_redirect!
     assert_template 'users/show'
     assert_select 'a[href=?]', login_path, count: 0
     assert_select 'a[href=?]', logout_path
     assert_select 'a[href=?]', user_path(@user)
+  end
+end
 
+class Logout < ValidLogin
+  def setup
+    super
     delete logout_path
+  end
+end
+
+class LogoutTest < Logout
+  test 'successful logout' do
     assert_not is_logged_in?
     assert_response :see_other
     assert_redirected_to root_url
+  end
+
+  test 'redirected after logout' do
     follow_redirect!
     assert_select 'a[href=?]', login_path
     assert_select 'a[href=?]', logout_path, count: 0
