c7f4130247f02d6022fae6488064eb03bc042987
oh84 <100363393+oh84@users.noreply.github.com>
2024-08-16 15:35:08 +0900

    記憶トークンをCookieに保存してログイン状態を保持できるように

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       reset_session # セッション固定への対策
+      remember user
       log_in user
       redirect_to user
     else
