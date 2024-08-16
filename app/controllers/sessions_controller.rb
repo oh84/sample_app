43d057a240cb8a1db8210f74d43c126f35d6ce3a
oh84 <100363393+oh84@users.noreply.github.com>
2024-08-16 16:36:17 +0900

    ログイン時に記憶トークンを保存するかチェックボックスで選択できるように

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       reset_session # セッション固定への対策
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else
