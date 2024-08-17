947f30659adc68b7374acc773b1c797554d14f11
oh84 <100363393+oh84@users.noreply.github.com>
2024-08-17 19:11:39 +0900

    フレンドリーフォワーディングのテストを追加

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -22,9 +22,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert', text: 'The form contains 3 errors.'
   end
 
-  test 'successful edit' do
-    log_in_as(@user)
+  test 'successful edit with friendly forwarding' do
     get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
     assert_template 'users/edit'
     name = 'Foo Bar'
     email = 'foo@bar.com'
