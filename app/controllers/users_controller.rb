57ac72dd9139fad9a95aa783fb2c68e6f9cfdb05
oh84 <100363393+oh84@users.noreply.github.com>
2024-08-18 17:03:08 +0900

    Web経由でユーザーのadmin属性を変更できないことをテスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,12 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 
   # beforeフィルタ
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,19 +1,45 @@
-require "test_helper"
+require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = 'Ruby on Rails Tutorial Sample App'
+    @user = users(:michael)
+    @other_user = users(:archer)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get signup_path
     assert_response :success
-    assert_select "title", "Sign up | #{@base_title}"
+    assert_select 'title', "Sign up | #{@base_title}"
   end
 
-  test "should redirect index when not logged in" do
+  test 'should redirect index when not logged in' do
     get users_path
     assert_redirected_to login_url
+  end
+
+  test 'should redirect update when not logged in' do
+    patch user_path(@user), params: {
+      user: {
+        name: @user.name,
+        email: @user.email
+      }
+    }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: {
+        password: 'password',
+        password_confirmation: 'password',
+        admin: true
+      }
+    }
+    assert_not @other_user.reload.admin?
   end
 end
