e93b1e659def4200be2e5458aa156269ce05edd7
oh84 <100363393+oh84@users.noreply.github.com>
2024-08-18 15:48:25 +0900

    ユーザー一覧ページを追加

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,4 +11,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Sign up | #{@base_title}"
   end
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
 end
