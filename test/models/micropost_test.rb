32bbcff3ef2908a638344176dd8f7f7a122371b5
oh84 <100363393+oh84@users.noreply.github.com>
2024-08-25 17:24:37 +0900

    テストを書く場所を間違えていたので修正

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
   def setup
@@ -27,13 +27,5 @@ class MicropostTest < ActiveSupport::TestCase
 
   test 'order should be most recent first' do
     assert_equal microposts(:most_recent), Micropost.first
-  end
-
-  test 'associated microposts should be destroyed' do
-    @user.save
-    @user.microposts.create!(content: 'Lorem ipsum')
-    assert_difference 'Micropost.count', -1 do
-      @user.destroy
-    end
   end
 end
