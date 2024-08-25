3f191ed3a8708e469effe77ae14cd13f35d5b2a7
oh84 <100363393+oh84@users.noreply.github.com>
2024-08-25 21:30:02 +0900

    マイクロポスト投稿フォームを作成

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
 
   resources :account_activations, only: %i[edit]
   resources :password_resets,     only: %i[new create edit update]
+
+  resources :microposts, only: %i[create destroy]
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -40,7 +40,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', edit_user_path(@user)
     assert_select 'a[href=?]', logout_path
 
-    assert_select 'a[href=?]', signup_path
+    assert_select 'a[href=?]', signup_path, count: 0
 
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
