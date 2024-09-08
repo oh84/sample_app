a965ab1b0be4c5894c1fa08dc0b48753e2ae67a3
oh84 <100363393+oh84@users.noreply.github.com>
2024-09-09 00:10:24 +0900

    フィード取得処理のN+1クエリ問題に対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -78,6 +78,7 @@ class User < ApplicationRecord
   def feed
     following_ids = 'SELECT followed_id FROM relationships WHERE follower_id = :user_id'
     Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+             .includes(:user, image_attachment: :blob)
   end
 
   def follow(other_user)
