9197f59c6f62385a2f2b35fe096aff43ddde5b81
oh84 <100363393+oh84@users.noreply.github.com>
2024-09-08 17:05:34 +0900

    フォローの統計情報を追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
 
   get '/signup', to: 'users#new'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   get '/login',     to: 'sessions#new'
   post '/login',    to: 'sessions#create'
