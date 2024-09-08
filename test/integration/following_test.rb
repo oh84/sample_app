be1ebb0d0ff4245da2686b111e6c8ad7f2e5378e
oh84 <100363393+oh84@users.noreply.github.com>
2024-09-09 01:21:08 +0900

    フィードの統合テストを追加

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -1,5 +1,4 @@
 require 'test_helper'
-
 class Following < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
@@ -26,6 +25,13 @@ class FollowPagesTest < Following
     assert_match @user.followers.count.to_s, response.body
     @user.followers.each do |user|
       assert_select 'a[href=?]', user_path(user)
+    end
+  end
+
+  test 'feed on Home page' do
+    get root_path
+    @user.feed.paginate(page: 1).each do |micropost|
+      assert_match CGI.escapeHTML(micropost.content), response.body
     end
   end
 end
